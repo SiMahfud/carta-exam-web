bf57abc11ee2d716581d19fefe02a0a374a00e18
SiMahfud <simahfud@sman1campurdarat.sch.id>
2026-08-17 13:15:51 +0700

    chore: configure PM2 to run Next.js on port 3333 with local storage persistence and ignore generated storage files

--- pm2.config.js
+++ pm2.config.js
@@ -2,8 +2,8 @@ module.exports = {
     apps: [
         {
             name: 'carta-exam',
-            script: 'npm',
-            args: 'start',
+            script: 'node_modules/next/dist/bin/next',
+            args: 'start -p 3333',
             exec_mode: 'fork',
             instances: 1,
             autorestart: true,
@@ -11,6 +11,7 @@ module.exports = {
             max_memory_restart: '1G',
             env: {
                 NODE_ENV: 'production',
+                NODE_OPTIONS: '--localstorage-file=./.localstorage.json',
             },
         },
     ],
